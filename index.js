057f6df82beeaa87b58add9ce6c7d98ccf46a211
sidney.silva <sidney.silva@sepog.fortaleza.ce.gov.br>
2021-11-30 10:45:40 -0300

    Fin sección 8

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const path = require('path');
+require('dotenv').config();
+
+require('./database/config').dbConnection();
+
+const app = express();
+
+app.use(express.json());
+
+const server = require('http').createServer(app);
+module.exports.io     = require('socket.io')(server);
+require('./sockets/socket');
+
+const publicPath = path.resolve(__dirname, 'public');
+
+app.use(express.static(publicPath));
+
+app.use('/api/login', require('./routes/auth'));
+
+
+server.listen(process.env.PORT, (err) => {
+    if (err) throw new Error(err);
+    console.log('Servidor corriendo en puerto', process.env.PORT);
+});
